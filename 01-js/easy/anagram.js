6c383924b735385f220470890c00e690059d65c2
Harsh Vishwakarma <113020594+Harsh-2212@users.noreply.github.com>
2024-04-07 11:24:24 +0530

    Update anagram.js
    In this code the function isAnagram takes to string, and then it checks the length of both the string after checking the string length, it then uses the charMap object to verify the strings used in both the strings and after then both the strings are matched.

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,33 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.replace('/\s/g','').toLowerCase();
+  str2 = str1.replace('/\s/g','').toLowerCase();
 
+  if(str1.length !== str2.length){
+    return false;
+  }
+
+  const charMap1 = {};
+  const charMap2 = {};
+
+  for (let char of str1){
+    charMap1[char] = (charMap1[char] || 0) + 1;
+  }
+
+  for (let char of str2){
+    charMap2[char] = (charMap2[char] || 0) + 1;
+  }
+
+  for (let char in charMap1){
+    if (charMap1[char] !== charMap2[char])
+      return false;
+  }
+    return true;
 }
+const str1 = "care"
+const str2 = "race"
+
+console.log(isAnagram(str1,str2));
 
 module.exports = isAnagram;
